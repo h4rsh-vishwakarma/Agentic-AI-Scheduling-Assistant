dbd7c1622f97b1ed3cd9a4017b5baa1a47d633c9
Harsh Vishwakarma <144579460+h4rsh-vishwakarma@users.noreply.github.com>
2025-09-05 15:50:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from datetime import timedelta, datetime, date
 from agents.calendar_agent import CalendarAgent
 from agents.negotiator import negotiate_meeting
-from agents.suggestion_agent import suggest_meeting_slot   
+from agents.suggestion_agent import suggest_meeting_slot    
 from agents.llm_agent import get_llm_response  # ✅ LLM
 from main_utils import find_common_slots, find_partial_slots, format_slot
 
